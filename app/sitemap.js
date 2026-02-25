bcbe870e026f23d37516245d8ea60041fef1719d
pranay-parmarketing <pranay.badgujar@parmarketing.agency>
2026-02-25 10:48:15 +0530

    sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,9 @@
+// app/sitemap.js
+export default function sitemap() {
+    return [
+        {
+            url: 'https://yashasviprasad.com/',
+            lastModified: new Date(),
+        },
+    ];
+}
