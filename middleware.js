0f4f9cd47329ad666e943ad83bb5805bbe186ce9
pranay-parmarketing <pranay.badgujar@parmarketing.agency>
2026-02-27 15:03:02 +0530

    live

--- middleware.js
+++ middleware.js
@@ -1,15 +1,26 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//     const token = request.cookies.get("token")?.value;
+
+//     if (!token && request.nextUrl.pathname.startsWith("/admin")) {
+//         return NextResponse.redirect(new URL("/login", request.url));
+//     }
+
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ["/admin/:path*"],
+// };
+
+import { authMiddleware } from "./lib/authMiddleware";
 
 export function middleware(request) {
-    const token = request.cookies.get("token")?.value;
-
-    if (!token && request.nextUrl.pathname.startsWith("/admin")) {
-        return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    return NextResponse.next();
+    return authMiddleware(request);
 }
 
+// Tell Next.js which routes to apply this middleware to
 export const config = {
     matcher: ["/admin/:path*"],
 };
